af2fca08b081bb2f6738bf71d9593496df74d147
nalchevanidze <d.nalchevanidze@gmail.com>
2018-04-02 10:55:45 +0200

    fix(main): terminate threat on exit

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,13 @@ import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import panel.Keyboard;
 import sound.SoundProcessor;
 
 public class Main extends Application {
+
+    private boolean _isLive = true;
 
     public static void main(String[] args) {
         launch(args);
@@ -20,7 +23,7 @@ public class Main extends Application {
         can.paint();
         layout.getChildren().add(can);
 
-        Scene scene = new Scene(layout, 200, 300);
+        Scene scene = new Scene(layout, 720, 190);
         primaryStage.setScene(scene);
         primaryStage.show();
 
@@ -36,12 +39,17 @@ public class Main extends Application {
             sound.stop();
         });
 
+        primaryStage.setOnCloseRequest((WindowEvent event)-> {
+            _isLive = false;
+        });
+
         Thread soundRunner = new Thread(()->{
                 try {
-                    while (true) {
+                    while (_isLive) {
                         sound.next();
                         Thread.sleep(120);
                     }
+                    sound.terminate();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
