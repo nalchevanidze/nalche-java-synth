17d862001016c998d77f60a1ae0c445bf04fdc24
davit <daviti@davnals-MBP.cmh>
2018-06-13 23:59:49 +0200

    remap notes

--- src/panel/Note.kt
+++ src/panel/Note.kt
@@ -28,33 +28,32 @@ class Note {
 
     fun noteFromKeyEvent(event: KeyEvent): Int {
         val key = event.code.getName().toLowerCase()[0]
-
-        when (key) {
-            'y' -> return 1
-            's' -> return 2
-            'x' -> return 3
-            'd' -> return 4
-            'c' -> return 5
-            'v' -> return 6
-            'g' -> return 7
-            'b' -> return 8
-            'h' -> return 9
-            'n' -> return 10
-            'j' -> return 11
-            'm' -> return 12
-            'q' -> return 13
-            '2' -> return 14
-            'w' -> return 15
-            '3' -> return 16
-            'e' -> return 17
-            'r' -> return 18
-            '5' -> return 19
-            't' -> return 20
-            '6' -> return 21
-            'z' -> return 22
-            '7' -> return 23
-            'u' -> return 24
+        return when (key) {
+            'y' ->  1
+            's' ->  2
+            'x' ->  3
+            'd' ->  4
+            'c' ->  5
+            'v' ->  6
+            'g' ->  7
+            'b' ->  8
+            'h' ->  9
+            'n' ->  10
+            'j' ->  11
+            'm' ->  12
+            'q' ->  13
+            '2' ->  14
+            'w' ->  15
+            '3' ->  16
+            'e' ->  17
+            'r' ->  18
+            '5' ->  19
+            't' ->  20
+            '6' ->  21
+            'z' ->  22
+            '7' ->  23
+            'u' ->  24
+            else -> -1
         }
-        return -1
     }
 }
