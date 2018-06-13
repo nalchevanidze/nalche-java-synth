279c8e128fb060db1847451ffb872d8b83deab9b
davit <daviti@davnals-MBP.cmh>
2018-06-13 22:45:40 +0200

    wave square

--- src/sound/Wave.kt
+++ src/sound/Wave.kt
@@ -4,7 +4,7 @@ fun sine(waveIndex: Double) = Math.sin(Math.toRadians(waveIndex))
 
 fun saw(waveIndex: Double) = waveIndex / 180 - 1
 
-fun square(waveIndex: Double) = if (waveIndex > 180) -1 else 1
+fun square(waveIndex: Double) = Math.round(waveIndex/360)
 
 fun mixed(i: Double) = (square(i) / 2 + saw(i) / 2 + sine(i)) / 3
 
